19faeaf04107bb2f336aaaa1ef23871baab28803
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2025-09-29 11:45:33 +0700

    Add Supabase integration with client and middleware setup

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,51 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  const supabaseResponse = NextResponse.next({
+    request,
+  });
+
+  // // With Fluid compute, don't put this client in a global environment
+  // // variable. Always create a new one on each request.
+  // const supabase = createServerClient(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+  //   {
+  //     cookies: {
+  //       getAll() {
+  //         return request.cookies.getAll();
+  //       },
+  //       setAll(cookiesToSet) {
+  //         cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
+  //         supabaseResponse = NextResponse.next({
+  //           request,
+  //         });
+  //         cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
+  //       },
+  //     },
+  //   }
+  // );
+
+  // // Do not run code between createServerClient and
+  // // supabase.auth.getClaims(). A simple mistake could make it very hard to debug
+  // // issues with users being randomly logged out.
+
+  // // IMPORTANT: If you remove getClaims() and you use server-side rendering
+  // // with the Supabase client, your users may be randomly logged out.
+  // const { data } = await supabase.auth.getClaims();
+  // const user = data?.claims;
+
+  // if (
+  //   !user &&
+  //   request.nextUrl.pathname.startsWith("/dashboard") &&
+  //   !request.nextUrl.pathname.startsWith("/login") &&
+  //   !request.nextUrl.pathname.startsWith("/auth")
+  // ) {
+  //   // no user, potentially respond by redirecting the user to the login page
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = "/auth/login";
+  //   return NextResponse.redirect(url);
+  // }
+
+  return supabaseResponse;
+}
